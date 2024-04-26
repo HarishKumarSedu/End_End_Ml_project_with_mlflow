9f97218ce1ce24fa0978cb5f7da188d1eb5cb82f
HarishKumarS <harishkumarsedu@gmail.com>
2024-04-26 13:00:41 +0200

    Test

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ COPY . /app
 WORKDIR /app
 RUN pip install --upgrade pip 
 RUN pip install -r requirements.txt
-EXPOSE $PORT
-CMD waitress-serve --listen=127.0.0.1:$PORT app:app
-# CMD gunicorn --workers=4 --bind 0.0.0.0:$PORT app:app
+EXPOSE 5000
+# CMD waitress-serve --listen=127.0.0.1:5000 app:app
+CMD gunicorn --workers=4 --bind 0.0.0.0:$PORT app:app
+# CMD python app.py 
--- src/mlProject/components/model_evaluation.py
+++ src/mlProject/components/model_evaluation.py
@@ -33,36 +33,44 @@ class ModelEvaluation:
         test_y = test_data[[self.config.target_column]]
 
 
-        mlflow.set_registry_uri(self.config.mlflow_uri)
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        # mlflow.set_registry_uri(self.config.mlflow_uri)
+        # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
 
-        with mlflow.start_run():
+        # with mlflow.start_run():
 
-            predicted_qualities = model.predict(test_x)
+            # predicted_qualities = model.predict(test_x)
 
-            (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
+            # (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
             
-            # Saving metrics as local
-            scores = {"rmse": rmse, "mae": mae, "r2": r2}
-            save_json(path=Path(self.config.metric_file_name), data=scores)
+            # # Saving metrics as local
+            # scores = {"rmse": rmse, "mae": mae, "r2": r2}
+            # save_json(path=Path(self.config.metric_file_name), data=scores)
 
-            mlflow.log_params(self.config.all_params)
+            # mlflow.log_params(self.config.all_params)
 
-            mlflow.log_metric("rmse", rmse)
-            mlflow.log_metric("r2", r2)
-            mlflow.log_metric("mae", mae)
+            # mlflow.log_metric("rmse", rmse)
+            # mlflow.log_metric("r2", r2)
+            # mlflow.log_metric("mae", mae)
 
 
             # Model registry does not work with file store
-            if tracking_url_type_store != "file":
+            # if tracking_url_type_store != "file":
 
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
-            else:
-                mlflow.sklearn.log_model(model, "model")
+            #     # Register the model
+            #     # There are other ways to use the Model Registry, which depends on the use case,
+            #     # please refer to the doc for more information:
+            #     # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            #     mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+            # else:
+            #     mlflow.sklearn.log_model(model, "model")
+            
+        predicted_qualities = model.predict(test_x)
+        (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
+        
+        # Saving metrics as local
+        scores = {"rmse": rmse, "mae": mae, "r2": r2}
+        save_json(path=Path(self.config.metric_file_name), data=scores)
+
 
     
